39a45e01765b326ea3e8c8a6f27d944977519d29
Daniel Bennington <dbennington125@gmail.com>
2020-05-19 10:21:57 -0700

    fixed title

--- src/GamePage.js
+++ src/GamePage.js
@@ -5,20 +5,24 @@ export default class GamePage extends Component {
    state= {
        data: [],
        currentQuestion: '',
+       title: '',
    }
    
    componentDidMount = async() => {
        const data = await request.get(`http://jservice.io/api/random?count=1`
        )
        console.log(data.body[0].category.title)
-       this.setState({ currentQuestion: data.body[0] })
+       this.setState({ 
+           currentQuestion: data.body[0],
+           title: data.body[0].category.title
+        })
    }
 
     render() {
-       const { currentQuestion } = this.state
+       const { currentQuestion, title } = this.state
         return (
             <div>
-                {/* <p>{currentQuestion['category']['title']}</p> */}
+                <p>{title}</p>
                 <p>{currentQuestion.question}</p>
             </div>
         )
