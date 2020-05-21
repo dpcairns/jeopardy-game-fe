68d4388dbd0b87632ee5fa5a5cfa208911235f19
JoLeaper <joleap123@gmail.com>
2020-05-20 17:01:14 -0700

    Updates

---
+++ precache-manifest.76cc479b26466c002897949ab8e287e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68779dab3ab51cea9de6e8b75fc77a74",
+    "url": "/jeopardy-game-fe/index.html"
+  },
+  {
+    "revision": "229dc2cd04bf6536d21e",
+    "url": "/jeopardy-game-fe/static/css/main.e7b6b4f9.chunk.css"
+  },
+  {
+    "revision": "4cd4f0b4f1748cc2ad5a",
+    "url": "/jeopardy-game-fe/static/js/2.2a4e2dd2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jeopardy-game-fe/static/js/2.2a4e2dd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "229dc2cd04bf6536d21e",
+    "url": "/jeopardy-game-fe/static/js/main.5bfc1666.chunk.js"
+  },
+  {
+    "revision": "0b3104e45705d83ab872",
+    "url": "/jeopardy-game-fe/static/js/runtime-main.2f2f362a.js"
+  }
+]);
